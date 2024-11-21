921978876c681b75bba12141e3011a3c94a3b80d
cortez <89983388+cortez24rus@users.noreply.github.com>
2024-11-21 15:36:27 +0700

    Update marz-rp-install-server.sh

--- other/marz-rp-install-server.sh
+++ other/marz-rp-install-server.sh
@@ -790,7 +790,8 @@ server {
     add_header X-XSS-Protection          "1; mode=block" always;
     add_header X-Content-Type-Options    "nosniff" always;
     add_header Referrer-Policy           "no-referrer-when-downgrade" always;
-    add_header Content-Security-Policy   "default-src 'self'; script-src 'self' 'unsafe-inline'; object-src 'none';" always;
+#    add_header Content-Security-Policy   "default-src 'self'; script-src 'self' 'unsafe-inline'; object-src 'none';" always;
+#    add_header Content-Security-Policy   "default-src 'self'; script-src 'self'; style-src 'self'; img-src 'self'; connect-src 'self'; font-src 'self'; object-src 'none'; frame-ancestors 'self';" always;
     add_header Permissions-Policy        "interest-cohort=()" always;
     add_header Strict-Transport-Security "max-age=31536000; includeSubDomains; preload" always;
     add_header X-Frame-Options           "SAMEORIGIN";
